b268ef89f76921781c448597c3e1914efdba060f
Paul Betts <paul@paulbetts.org>
2012-03-24 14:46:17 -0700

    Implement the stubbed GitHub API methods

--- ReactiveUI.Sample/Models/GitHubApi.cs
+++ ReactiveUI.Sample/Models/GitHubApi.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reactive;
+using System.Reactive.Linq;
 using System.Text;
 using Ninject;
+using ReactiveUI.Sample.Helpers;
 using RestSharp;
 
 namespace ReactiveUI.Sample.Models
@@ -16,19 +18,24 @@ namespace ReactiveUI.Sample.Models
 
     public class GitHubApi : IGitHubApi
     {
+        readonly IRestClient client;
+
         [Inject]
         public GitHubApi(IRestClient authedClient)
         {
+            client = authedClient;
         }
 
         public IObservable<List<GitHubOrgInfo>> GetOrganizationsForUser()
         {
-            throw new NotImplementedException();
+            var req = new RestRequest("user/orgs");
+            return client.RequestAsync<List<GitHubOrgInfo>>(req).Select(x => x.Data);
         }
 
         public IObservable<List<GitHubRepo>> GetReposForUser()
         {
-            throw new NotImplementedException();
+            var req = new RestRequest("user/repos");
+            return client.RequestAsync<List<GitHubRepo>>(req).Select(x => x.Data);
         }
     }
 }
