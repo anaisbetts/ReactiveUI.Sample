c91d037052a5c1a15d482d96389a2e5acea52ffa
Paul Betts <paul@paulbetts.org>
2012-03-24 14:28:14 -0700

    Shuffle some data around in RepoSelectionViewModel

--- ReactiveUI.Sample/ViewModels/RepoSelectionViewModel.cs
+++ ReactiveUI.Sample/ViewModels/RepoSelectionViewModel.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reactive.Linq;
 using System.Windows.Media;
 using Ninject;
@@ -8,11 +9,6 @@ using ReactiveUI.Sample.Models;
 
 namespace ReactiveUI.Sample.ViewModels
 {
-    public interface IRepoSelectionViewModel : IRoutableViewModel
-    {
-        ReactiveCollection<OrganizationTileViewModel> Organizations { get; }
-    }
-
     public class RepoTileViewModel : ReactiveObject
     {
         public GitHubRepo Model { get; set; }
@@ -20,24 +16,35 @@ namespace ReactiveUI.Sample.ViewModels
 
     public class OrganizationTileViewModel : ReactiveObject
     {
+        public GitHubOrgInfo Model { get; set; }
         public ImageSource Image { get; set; }
         public string Name { get; set; }
 
         public ReactiveCollection<RepoTileViewModel> Repositories { get; protected set; }
 
-        public OrganizationTileViewModel(IObservable<List<RepoTileViewModel>> repositories)
+        public OrganizationTileViewModel(GitHubOrgInfo model, IObservable<List<GitHubRepo>> repositories)
         {
             Repositories = new ReactiveCollection<RepoTileViewModel>();
 
             repositories
+                .SelectMany(x => x.ToObservable())
                 .ObserveOn(RxApp.DeferredScheduler)
-                .Subscribe(x => x.ForEach(Repositories.Add));
+                .Where(x => model.login == x.owner.login)
+                .Subscribe(x => Repositories.Add(new RepoTileViewModel() { Model = x }));
         }
+    }
+
+    public interface IRepoSelectionViewModel : IRoutableViewModel
+    {
+        ReactiveCollection<OrganizationTileViewModel> Organizations { get; }
     }
 
     public class RepoSelectionViewModel : ReactiveObject, IRepoSelectionViewModel
     {
-        public ReactiveCollection<OrganizationTileViewModel> Organizations { get; protected set; }
+        ObservableAsPropertyHelper<ReactiveCollection<OrganizationTileViewModel>> _Organizations;
+        public ReactiveCollection<OrganizationTileViewModel> Organizations {
+            get { return _Organizations.Value;  }
+        }
 
         public string UrlPathSegment {
             get { return "repos"; }
@@ -46,10 +53,21 @@ namespace ReactiveUI.Sample.ViewModels
         public IScreen HostScreen { get; protected set; }
 
         [Inject]
-        public RepoSelectionViewModel(IScreen hostScreen)
+        public RepoSelectionViewModel(IScreen hostScreen, IGitHubApi api)
         {
             HostScreen = hostScreen;
-            Organizations = new ReactiveCollection<OrganizationTileViewModel>();
+
+            var repos = api.GetReposForUser();
+
+            api.GetOrganizationsForUser()
+                .Select(x => createOrgViewModels(x, repos))
+                .ToProperty(this, x => x.Organizations);
+        }
+
+        ReactiveCollection<OrganizationTileViewModel> createOrgViewModels(IEnumerable<GitHubOrgInfo> gitHubOrgInfos, IObservable<List<GitHubRepo>> repos)
+        {
+            return new ReactiveCollection<OrganizationTileViewModel>(
+                gitHubOrgInfos.Select(x => new OrganizationTileViewModel(x, repos)));
         }
     }
 }
