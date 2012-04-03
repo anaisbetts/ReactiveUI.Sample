9ab3a97c4e3513a3a7007b5bd815f56481ca4e8e
Paul Betts <paul@paulbetts.org>
2012-04-02 19:17:48 -0700

    Add GetCurrentUser to GitHubApis

--- ReactiveUI.Sample/Models/GitHubApi.cs
+++ ReactiveUI.Sample/Models/GitHubApi.cs
@@ -14,11 +14,14 @@ namespace ReactiveUI.Sample.Models
     {
         IObservable<List<GitHubOrgInfo>> GetOrganizationsForUser();
         IObservable<List<GitHubRepo>> GetReposForUser();
+        IObservable<GitHubUser> GetCurrentUser();
     }
 
     public class GitHubApi : IGitHubApi
     {
         readonly IRestClient client;
+
+        public string UserName { get; set; }
 
         [Inject]
         public GitHubApi(IRestClient authedClient)
@@ -36,6 +39,12 @@ namespace ReactiveUI.Sample.Models
         {
             var req = new RestRequest("user/repos");
             return client.RequestAsync<List<GitHubRepo>>(req).Select(x => x.Data);
+        }
+
+        public IObservable<GitHubUser> GetCurrentUser()
+        {
+            var req = new RestRequest("user");
+            return client.RequestAsync<GitHubUser>(req).Select(x => x.Data);
         }
     }
 }
